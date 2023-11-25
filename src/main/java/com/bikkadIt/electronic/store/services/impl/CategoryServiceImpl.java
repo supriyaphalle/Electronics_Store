cbdf0baa373354f66221e0881467c90fb6021c19
supriyaphalle <supriyaphalle@gmail.com>
2023-11-25 20:26:17 +0530

    Added: category serviceImpl class

---
+++ src/main/java/com/bikkadIt/electronic/store/services/impl/CategoryServiceImpl.java
@@ -0,0 +1,32 @@
+package com.bikkadIt.electronic.store.services.impl;
+
+import com.bikkadIt.electronic.store.dtos.CategoryDto;
+import com.bikkadIt.electronic.store.dtos.PageableResponse;
+import com.bikkadIt.electronic.store.services.CategoryService;
+
+public class CategoryServiceImpl implements CategoryService {
+    @Override
+    public CategoryDto create(CategoryDto categoryDto) {
+        return null;
+    }
+
+    @Override
+    public CategoryDto update(CategoryDto categoryDto, String categoryId) {
+        return null;
+    }
+
+    @Override
+    public void delete(String categoryId) {
+
+    }
+
+    @Override
+    public PageableResponse<CategoryDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public CategoryDto get(String categoryId) {
+        return null;
+    }
+}
