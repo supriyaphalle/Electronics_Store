8dd290db97eb4dd42287478ffb280e574039d4e3
supriyaphalle <supriyaphalle@gmail.com>
2023-11-25 21:41:07 +0530

    Added: Http request to get all category data with pagination

--- src/main/java/com/bikkadIt/electronic/store/controllers/CategoryController.java
+++ src/main/java/com/bikkadIt/electronic/store/controllers/CategoryController.java
@@ -3,6 +3,7 @@ package com.bikkadIt.electronic.store.controllers;
 import com.bikkadIt.electronic.store.constant.AppConstants;
 import com.bikkadIt.electronic.store.dtos.ApiResponseMessage;
 import com.bikkadIt.electronic.store.dtos.CategoryDto;
+import com.bikkadIt.electronic.store.dtos.PageableResponse;
 import com.bikkadIt.electronic.store.services.CategoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,16 +34,29 @@ public class CategoryController {
         logger.info("Entering Request to update category Data for id:{}", categoryId);
         CategoryDto update = categoryService.update(categoryDto, categoryId);
         logger.info("Entering Request to update category Data for id:{}", categoryId);
-        return  new ResponseEntity<CategoryDto>(update,HttpStatus.OK);
+        return new ResponseEntity<CategoryDto>(update, HttpStatus.OK);
     }
 
     @DeleteMapping("/{categoryId}")
-    public ResponseEntity<ApiResponseMessage>deleteData(@PathVariable String categoryId){
-        logger.info("Entering request to delete category data for id:{}",categoryId);
+    public ResponseEntity<ApiResponseMessage> deleteData(@PathVariable String categoryId) {
+        logger.info("Entering request to delete category data for id:{}", categoryId);
         categoryService.delete(categoryId);
         ApiResponseMessage message = new ApiResponseMessage(AppConstants.DELETE_RESPONSE, true, HttpStatus.OK);
-        logger.info("Completed request to delete category data for id:{}",categoryId);
-        return new ResponseEntity<ApiResponseMessage>(message,HttpStatus.OK);
+        logger.info("Completed request to delete category data for id:{}", categoryId);
+        return new ResponseEntity<ApiResponseMessage>(message, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<PageableResponse> getAllData(
+            @RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) int pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR, required = false) String sortDir
+    ) {
+        logger.info("Entering request to delete category data ");
+        PageableResponse<CategoryDto> allList = categoryService.getAll(pageNumber, pageSize, sortBy, sortDir);
+        logger.info("Completed request to delete category data ");
+        return new ResponseEntity<PageableResponse>(allList,HttpStatus.OK);
     }
 
 
