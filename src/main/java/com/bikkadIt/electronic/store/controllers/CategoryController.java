24df960f7614da2e3870cc358f25ae1ce373c7e6
supriyaphalle <supriyaphalle@gmail.com>
2023-11-25 21:57:33 +0530

    Added : Document comments

--- src/main/java/com/bikkadIt/electronic/store/controllers/CategoryController.java
+++ src/main/java/com/bikkadIt/electronic/store/controllers/CategoryController.java
@@ -21,14 +21,28 @@ public class CategoryController {
 
     Logger logger = LoggerFactory.getLogger(CategoryController.class);
 
+    /**
+     * @param categoryDto
+     * @return http status for save data
+     * @author SUPRIYA
+     * @apiNote To save category data in  database
+     * @since V 1.0
+     */
     @PostMapping
     public ResponseEntity<CategoryDto> createData(@RequestBody CategoryDto categoryDto) {
         logger.info("Entering Request to create category Data");
         CategoryDto category = categoryService.create(categoryDto);
         logger.info("Completed Request to create category data");
-        return new ResponseEntity(category, HttpStatus.CREATED);
+        return new ResponseEntity<>(category, HttpStatus.CREATED);
     }
 
+    /**
+     * @param categoryDto , categoryId
+     * @return http status for save data
+     * @author SUPRIYA
+     * @apiNote To update category data in  database
+     * @since V 1.0
+     */
     @PutMapping("/{categoryId}")
     public ResponseEntity<CategoryDto> updateData(@RequestBody CategoryDto categoryDto, @PathVariable String categoryId) {
         logger.info("Entering Request to update category Data for id:{}", categoryId);
@@ -37,6 +51,12 @@ public class CategoryController {
         return new ResponseEntity<CategoryDto>(update, HttpStatus.OK);
     }
 
+    /**
+     * @return http status for delete data
+     * @author SUPRIYA
+     * @apiNote To delete category data in  database
+     * @since V 1.0
+     */
     @DeleteMapping("/{categoryId}")
     public ResponseEntity<ApiResponseMessage> deleteData(@PathVariable String categoryId) {
         logger.info("Entering request to delete category data for id:{}", categoryId);
@@ -46,8 +66,15 @@ public class CategoryController {
         return new ResponseEntity<ApiResponseMessage>(message, HttpStatus.OK);
     }
 
+    /**
+     * @param
+     * @return get all category data
+     * @author SUPRIYA
+     * @apiNote To get category data from  database
+     * @since V 1.0
+     */
     @GetMapping
-    public ResponseEntity<PageableResponse> getAllData(
+    public ResponseEntity<PageableResponse<CategoryDto>> getAllData(
             @RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) int pageNumber,
             @RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) int pageSize,
             @RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
@@ -56,9 +83,16 @@ public class CategoryController {
         logger.info("Entering request to delete category data ");
         PageableResponse<CategoryDto> allList = categoryService.getAll(pageNumber, pageSize, sortBy, sortDir);
         logger.info("Completed request to delete category data ");
-        return new ResponseEntity<PageableResponse>(allList, HttpStatus.OK);
+        return new ResponseEntity<>(allList, HttpStatus.OK);
     }
 
+    /**
+     * @return http status for update data
+     * @Param categoryID
+     * @author SUPRIYA
+     * @apiNote To get category data from database
+     * @since V 1.0
+     */
     @GetMapping("/categoryId")
     public ResponseEntity<CategoryDto> getCategoryDataById(@PathVariable String categoryId) {
         logger.info("Entering request to get category data for id:{}", categoryId);
