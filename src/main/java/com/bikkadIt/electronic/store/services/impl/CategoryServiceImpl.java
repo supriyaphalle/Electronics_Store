469dbb4c0a190b772f5f550595a337c855a22434
supriyaphalle <supriyaphalle@gmail.com>
2023-11-25 20:41:08 +0530

    Added: logic for save category data

--- src/main/java/com/bikkadIt/electronic/store/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/bikkadIt/electronic/store/services/impl/CategoryServiceImpl.java
@@ -2,12 +2,29 @@ package com.bikkadIt.electronic.store.services.impl;
 
 import com.bikkadIt.electronic.store.dtos.CategoryDto;
 import com.bikkadIt.electronic.store.dtos.PageableResponse;
+import com.bikkadIt.electronic.store.entities.Category;
+import com.bikkadIt.electronic.store.repositories.CategoryRepository;
 import com.bikkadIt.electronic.store.services.CategoryService;
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class CategoryServiceImpl implements CategoryService {
+
+    Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
+    @Autowired
+    private ModelMapper modelMapper;
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public CategoryDto create(CategoryDto categoryDto) {
-        return null;
+        logger.info("Initiating the dao call for the save category data");
+        Category category = modelMapper.map(categoryDto, Category.class);
+        Category save = categoryRepository.save(category);
+        logger.info("Completed the dao call for the save category data");
+        return modelMapper.map(save, CategoryDto.class);
     }
 
     @Override
