60c8b98fb2f5cc2c2f97afa6d6cf869831137e22
supriyaphalle <supriyaphalle@gmail.com>
2023-12-04 14:31:14 +0530

    Added : Test case for update use Api

--- src/test/java/com/bikkadIt/electronic/store/service/UserServiceTest.java
+++ src/test/java/com/bikkadIt/electronic/store/service/UserServiceTest.java
@@ -8,13 +8,13 @@ import com.bikkadIt.electronic.store.services.UserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
 
 @SpringBootTest
 public class UserServiceTest {
@@ -48,5 +48,34 @@ public class UserServiceTest {
         UserDto user1 = userService.createUser(mapper.map(user, UserDto.class));
        System.out.println(user1.getName());
         Assertions.assertNotNull(user1);
+
+        Assertions.assertEquals("Supriya",user1.getName());
     }
+
+
+    @Test
+    public void updateUserTest(){
+
+        String userID="qazxswedc";
+
+        UserDto userDto=UserDto.builder()
+                .name("Priya Kalekar")
+                .about("This is update data")
+                .gender("Female")
+                .imageName("abc.jpg")
+                .build();
+
+        Mockito.when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.of(user));
+        Mockito.when(userRepository.save(Mockito.any())).thenReturn(user);
+
+        UserDto updateUser = userService.updateUSer(userDto,userID);
+        System.out.println(updateUser.getName());
+        Assertions.assertNotNull(userDto);
+
+        Assertions.assertEquals(userDto.getName(),updateUser.getName(),"Name is not equal");
+
+
+
+    }
+
 }
