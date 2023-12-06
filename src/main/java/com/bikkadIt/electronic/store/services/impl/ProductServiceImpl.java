de92557e102d23369183f6fdabc3ecba844e02b3
supriyaphalle <supriyaphalle@gmail.com>
2023-12-06 22:19:09 +0530

    Added : Logic for save product data in ProductServiceImpl

--- src/main/java/com/bikkadIt/electronic/store/services/impl/ProductServiceImpl.java
+++ src/main/java/com/bikkadIt/electronic/store/services/impl/ProductServiceImpl.java
@@ -1,14 +1,28 @@
 package com.bikkadIt.electronic.store.services.impl;
 
 import com.bikkadIt.electronic.store.dtos.ProductDto;
+import com.bikkadIt.electronic.store.entities.Product;
+import com.bikkadIt.electronic.store.repositories.ProductRepository;
 import com.bikkadIt.electronic.store.services.ProductService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ModelMapper mapper;
+
+
     @Override
     public ProductDto create(ProductDto productDto) {
-        return null;
+        Product product= mapper.map(productDto,Product.class);
+        Product save = productRepository.save(product);
+        return mapper.map(save,ProductDto.class);
     }
 
     @Override
