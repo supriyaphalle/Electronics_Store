7dbceae2f640fa09fa5634829fbe4c89bce01e74
supriyaphalle <supriyaphalle@gmail.com>
2023-12-06 22:13:35 +0530

    Added : Implementation class (productserviceImpl) for ProductService

---
+++ src/main/java/com/bikkadIt/electronic/store/services/impl/ProductServiceImpl.java
@@ -0,0 +1,43 @@
+package com.bikkadIt.electronic.store.services.impl;
+
+import com.bikkadIt.electronic.store.dtos.ProductDto;
+import com.bikkadIt.electronic.store.services.ProductService;
+
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public ProductDto create(ProductDto productDto) {
+        return null;
+    }
+
+    @Override
+    public ProductDto update(ProductDto productDto, String productId) {
+        return null;
+    }
+
+    @Override
+    public void delete(String productId) {
+
+    }
+
+    @Override
+    public ProductDto get(String productId) {
+        return null;
+    }
+
+    @Override
+    public List<ProductDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<ProductDto> getAllLive() {
+        return null;
+    }
+
+    @Override
+    public List<ProductDto> searchByTitle(String subTitle) {
+        return null;
+    }
+}
