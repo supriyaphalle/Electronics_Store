8a008eb9f86af18058e6e24bf9e5d9b3fe8a48bf
supriyaphalle <supriyaphalle@gmail.com>
2023-12-06 22:00:50 +0530

    Added : Custom methods in product Repository

--- src/main/java/com/bikkadIt/electronic/store/repositories/ProductRepository.java
+++ src/main/java/com/bikkadIt/electronic/store/repositories/ProductRepository.java
@@ -4,6 +4,12 @@ import com.bikkadIt.electronic.store.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product,String> {
+
+    List<Product> findByTitleContaining(String subTitle);
+
+    List<Product> findByLiveTrue();
 }
