7173bb3e8ba42ece19c6effc39e2857767fc511e
supriyaphalle <supriyaphalle@gmail.com>
2023-12-07 22:27:24 +0530

    Added : loggers in ProductController

--- src/main/java/com/bikkadIt/electronic/store/controllers/ProductController.java
+++ src/main/java/com/bikkadIt/electronic/store/controllers/ProductController.java
@@ -5,6 +5,8 @@ import com.bikkadIt.electronic.store.dtos.ApiResponseMessage;
 import com.bikkadIt.electronic.store.dtos.PageableResponse;
 import com.bikkadIt.electronic.store.dtos.ProductDto;
 import com.bikkadIt.electronic.store.services.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,7 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
+    Logger logger = LoggerFactory.getLogger(ProductController.class);
 
     /**
      * @return http status for created data
@@ -29,7 +32,9 @@ public class ProductController {
      */
     @PostMapping("/")
     public ResponseEntity<ProductDto> createData(@Valid @RequestBody ProductDto productDto) {
+        logger.info("Entering Request to create product Data");
         ProductDto dto = productService.create(productDto);
+        logger.info("Completed Request to create product data");
         return new ResponseEntity<>(dto, HttpStatus.CREATED);
     }
 
@@ -43,9 +48,9 @@ public class ProductController {
      */
     @PutMapping("/{productId}")
     public ResponseEntity<ProductDto> updateProduct(@PathVariable String productId, @Valid @RequestBody ProductDto productDto) {
-
+        logger.info("Entering Request to update product Data for id:{}", productId);
         ProductDto update = productService.update(productDto, productId);
-
+        logger.info("Completed Request to update product Data for id:{}", productId);
         return new ResponseEntity<>(update, HttpStatus.OK);
 
     }
@@ -59,9 +64,10 @@ public class ProductController {
      */
     @DeleteMapping("/{productId}")
     public ResponseEntity<ApiResponseMessage> delete(@PathVariable String productId) {
+        logger.info("Entering Request to delete product Data for id:{}", productId);
         productService.delete(productId);
-
         ApiResponseMessage message = ApiResponseMessage.builder().message("Product Deleted successfully!!").success(true).status(HttpStatus.OK).build();
+        logger.info("Completed Request to delete product Data for id:{}", productId);
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
@@ -74,7 +80,9 @@ public class ProductController {
      */
     @GetMapping("/{productId}")
     public ResponseEntity<ProductDto> getProduct(@PathVariable String productId) {
+        logger.info("Entering request to get product data for id:{}", productId);
         ProductDto dto = productService.get(productId);
+        logger.info("Completed request to get product data for id:{}", productId);
         return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
@@ -92,10 +100,10 @@ public class ProductController {
             @RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
             @RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR, required = false) String sortDir
     ) {
-
+        logger.info("Entering request to get all product data ");
         PageableResponse<ProductDto> all = productService.getAll(pageNumber, pageSize, sortBy, sortDir);
+        logger.info("Completed request to get all product data ");
         return new ResponseEntity<>(all, HttpStatus.OK);
-
     }
 
     /**
@@ -112,10 +120,10 @@ public class ProductController {
             @RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
             @RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR, required = false) String sortDir
     ) {
-
+        logger.info("Entering request to get all live product data ");
         PageableResponse<ProductDto> all = productService.getAllLive(pageNumber, pageSize, sortBy, sortDir);
+        logger.info("Completed request to get all live  product data ");
         return new ResponseEntity<>(all, HttpStatus.OK);
-
     }
 
     /**
@@ -134,7 +142,9 @@ public class ProductController {
             @RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR, required = false) String sortDir
     ) {
 
+        logger.info("Entering request to search  product data by keyword:{}", keyword);
         PageableResponse<ProductDto> all = productService.searchByTitle(keyword, pageNumber, pageSize, sortBy, sortDir);
+        logger.info("Completed request to search  product data by keyword:{}", keyword);
         return new ResponseEntity<>(all, HttpStatus.OK);
 
     }
