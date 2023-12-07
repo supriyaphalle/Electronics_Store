f26e130a5ccedbfbb4bbb4fee6032d71a431cbda
supriyaphalle <supriyaphalle@gmail.com>
2023-12-07 21:50:50 +0530

    Added : update Api in ProductController

--- src/main/java/com/bikkadIt/electronic/store/controllers/ProductController.java
+++ src/main/java/com/bikkadIt/electronic/store/controllers/ProductController.java
@@ -1,14 +1,14 @@
 package com.bikkadIt.electronic.store.controllers;
 
+import com.bikkadIt.electronic.store.constant.AppConstants;
+import com.bikkadIt.electronic.store.dtos.ApiResponseMessage;
+import com.bikkadIt.electronic.store.dtos.PageableResponse;
 import com.bikkadIt.electronic.store.dtos.ProductDto;
 import com.bikkadIt.electronic.store.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -21,8 +21,26 @@ public class ProductController {
 
 
     @PostMapping("/")
-    public ResponseEntity<ProductDto> createData(@Valid @RequestBody ProductDto productDto){
+    public ResponseEntity<ProductDto> createData(@Valid @RequestBody ProductDto productDto) {
         ProductDto dto = productService.create(productDto);
         return new ResponseEntity<>(dto, HttpStatus.CREATED);
     }
+
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<ProductDto> updateProduct(@PathVariable String productId, @Valid @RequestBody ProductDto productDto) {
+
+        ProductDto update = productService.update(productDto, productId);
+
+        return new ResponseEntity<>(update, HttpStatus.OK);
+
+    }
+
+
+
+
+
+
+
+
 }
