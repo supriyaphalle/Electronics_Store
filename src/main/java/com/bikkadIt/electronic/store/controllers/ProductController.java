56e08471d1a7d417a2c1fece13e0fcaa47e11373
supriyaphalle <supriyaphalle@gmail.com>
2023-12-07 22:07:49 +0530

    Added : get all live product Api in ProductController

--- src/main/java/com/bikkadIt/electronic/store/controllers/ProductController.java
+++ src/main/java/com/bikkadIt/electronic/store/controllers/ProductController.java
@@ -62,7 +62,18 @@ public class ProductController {
 
     }
 
+    @GetMapping("/live")
+    public ResponseEntity<PageableResponse<ProductDto>> getAllLive(
+            @RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) int pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR, required = false) String sortDir
+    ) {
 
+        PageableResponse<ProductDto> all = productService.getAllLive(pageNumber, pageSize, sortBy, sortDir);
+        return new ResponseEntity<>(all, HttpStatus.OK);
+
+    }
 
 
 
