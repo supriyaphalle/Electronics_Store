dcda339a4ef157c7a05b2b32837a7f3818c6e166
supriyaphalle <supriyaphalle@gmail.com>
2023-12-08 22:06:09 +0530

    Refactored : column names of product entity

--- src/main/java/com/bikkadIt/electronic/store/entities/Product.java
+++ src/main/java/com/bikkadIt/electronic/store/entities/Product.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -20,13 +21,31 @@ public class Product {
 
     @Id
     private String productId;
-    private String title;
+
+    @Column(name="product_name")
+    private String name;
+
+    @Column(name="product_description")
     private String description;
+
+    @Column(name="product_price")
     private int price;
+
+    @Column(name="product_quantity")
     private int quantity;
+
+    @Column(name="product_date")
     private Date addedDate;
+
+    @Column(name="product_live")
     private boolean live;
+    @Column(name="product_stock")
     private boolean stock;
+
+    @Column(name="discount_Price")
     private int discountPrice;
+
+    @Column(name = "product_Image")
+    private String productImage;
 
 }
