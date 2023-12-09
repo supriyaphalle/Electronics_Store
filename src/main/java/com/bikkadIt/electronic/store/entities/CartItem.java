208d1a323066e7d090baee795e34e47b96790b63
supriyaphalle <supriyaphalle@gmail.com>
2023-12-09 23:58:59 +0530

    Added: CartItem

---
+++ src/main/java/com/bikkadIt/electronic/store/entities/CartItem.java
@@ -0,0 +1,30 @@
+package com.bikkadIt.electronic.store.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="cart_items")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cartItemId;
+
+    @OneToOne
+    private Product product;
+
+    private int quantity;
+
+    private int totalPrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+}
