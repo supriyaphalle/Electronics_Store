7b2de1d1bbbe7efff6d369c7b9d16d7af55b9c38
supriyaphalle <supriyaphalle@gmail.com>
2023-12-16 23:13:03 +0530

    Added: Implemented logic for OrderService abstract methods in OrderServiceImpl

---
+++ src/main/java/com/bikkadIt/electronic/store/services/impl/OrderServiceImpl.java
@@ -0,0 +1,120 @@
+package com.bikkadIt.electronic.store.services.impl;
+
+import com.bikkadIt.electronic.store.constant.AppConstants;
+import com.bikkadIt.electronic.store.dtos.CategoryDto;
+import com.bikkadIt.electronic.store.dtos.CreateOrderRequest;
+import com.bikkadIt.electronic.store.dtos.OrderDto;
+import com.bikkadIt.electronic.store.dtos.PageableResponse;
+import com.bikkadIt.electronic.store.entities.*;
+import com.bikkadIt.electronic.store.exceptions.BadApiRequestException;
+import com.bikkadIt.electronic.store.exceptions.ResourceNotFoundException;
+import com.bikkadIt.electronic.store.helper.Helper;
+import com.bikkadIt.electronic.store.repositories.CartRepository;
+import com.bikkadIt.electronic.store.repositories.OrderRepository;
+import com.bikkadIt.electronic.store.repositories.UserRepository;
+import com.bikkadIt.electronic.store.services.OrderService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Override
+    public OrderDto createOrder(CreateOrderRequest orderDto) {
+        String userId = orderDto.getUserId();
+        String cartId = orderDto.getCartId();
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.USER_NOT_FOUND));
+
+        Cart cart = cartRepository.findById(cartId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.CART_NOT_FOUND));
+
+        List<CartItem> cartItems = cart.getItems();
+
+        if (cartItems.size() <= 0) {
+            throw new BadApiRequestException("Invalid number of items in cart");
+        }
+
+        Order order = Order.builder()
+                .billingName(orderDto.getBillingName())
+                .billingPhone(orderDto.getBillingPhone())
+                .billingAddress(orderDto.getBillingAddress())
+                .orderDate(new Date())
+                .deliveredDate(null)
+                .paymentStatus(orderDto.getPaymentStatus())
+                .orderStatus(orderDto.getOrderStatus())
+                .orderId(UUID.randomUUID().toString())
+                .user(user)
+                .build();
+
+        AtomicReference<Integer> orderAmount = new AtomicReference<>(0);
+
+        List<OrderItem> orderItems = cartItems.stream().map(
+                cartItem -> {
+                    OrderItem orderItem = OrderItem.builder()
+                            .quantity(cartItem.getQuantity())
+                            .product(cartItem.getProduct())
+                            .totalPrice(cartItem.getQuantity() * cartItem.getProduct().getDiscountPrice())
+                            .order(order)
+                            .build();
+
+                    orderAmount.set(orderAmount.get() + orderItem.getTotalPrice());
+
+                    return orderItem;
+                }).collect(Collectors.toList());
+
+        order.setOrderItems(orderItems);
+        order.setOrderAmount(orderAmount.get());
+
+        cart.getItems().clear();
+        cartRepository.save(cart);
+        Order saveOrder = orderRepository.save(order);
+
+        return mapper.map(saveOrder, OrderDto.class);
+    }
+
+    @Override
+    public void removeOrder(String orderId) {
+
+        Order order = orderRepository.findById(orderId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.ORDER_NOT_FOUND));
+        orderRepository.delete(order);
+    }
+
+    @Override
+    public List<OrderDto> getOrderOfUser(String userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.USER_NOT_FOUND));
+        List<Order> orderList = orderRepository.findByUser(user);
+
+        List<OrderDto> dtoList = orderList.stream().map(order -> mapper.map(order, OrderDto.class)).collect(Collectors.toList());
+        return dtoList;
+    }
+
+    @Override
+    public PageableResponse<OrderDto> getOrders(int pageNumber, int pageSize, String sortBy, String sortDir) {
+
+        Sort sort = (sortDir.equalsIgnoreCase("desc")) ? (Sort.by(sortBy).descending()) : (Sort.by(sortBy).ascending());
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
+        Page<Order> all = orderRepository.findAll(pageable);
+        PageableResponse<OrderDto> response = Helper.getPageableResponse(all, OrderDto.class);
+        return response;
+    }
+}
