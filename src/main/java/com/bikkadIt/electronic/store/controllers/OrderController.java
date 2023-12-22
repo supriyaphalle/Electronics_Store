a7762834deaa1cf97cf95c9bcab8328798d5e747
supriyaphalle <supriyaphalle@gmail.com>
2023-12-22 15:45:20 +0530

    Added:loggers in OrderController

--- src/main/java/com/bikkadIt/electronic/store/controllers/OrderController.java
+++ src/main/java/com/bikkadIt/electronic/store/controllers/OrderController.java
@@ -2,7 +2,9 @@ package com.bikkadIt.electronic.store.controllers;
 
 import com.bikkadIt.electronic.store.dtos.*;
 import com.bikkadIt.electronic.store.services.OrderService;
-import lombok.Getter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,8 @@ public class OrderController {
     @Autowired
     OrderService orderService;
 
+    Logger logger = LoggerFactory.getLogger(OrderController.class);
+
     /**
      * @param orderRequest
      * @return newly created order
@@ -27,7 +31,9 @@ public class OrderController {
      */
     @PostMapping("/")
     public ResponseEntity<OrderDto> createOrder(@Valid @RequestBody CreateOrderRequest orderRequest) {
+        logger.info("Entering Request to create order Data");
         OrderDto orderDto = orderService.createOrder(orderRequest);
+        logger.info("Completed Request to create category Data");
         return new ResponseEntity<>(orderDto, HttpStatus.CREATED);
     }
 
@@ -40,9 +46,11 @@ public class OrderController {
      */
     @DeleteMapping("/{orderId}")
     public ResponseEntity<ApiResponseMessage> removeOrder(@PathVariable String orderId) {
+        logger.info("Entering request to delete order data for id:{}", orderId);
         orderService.removeOrder(orderId);
         ApiResponseMessage message = ApiResponseMessage.builder().status(HttpStatus.OK).message("order is removed!!")
                 .success(true).build();
+        logger.info("Completed request to delete order data for id:{}", orderId);
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
@@ -55,7 +63,9 @@ public class OrderController {
      */
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<OrderDto>> getOrderOfUser(@PathVariable String userId) {
+        logger.info("Entering request to get  order data for userID: {} ", userId);
         List<OrderDto> orderList = orderService.getOrderOfUser(userId);
+        logger.info("Completed request to get  order data for userID: {} ", userId);
         return new ResponseEntity<>(orderList, HttpStatus.OK);
     }
 
@@ -72,7 +82,9 @@ public class OrderController {
             @RequestParam(value = "sortBy", defaultValue = "billingName", required = false) String sortBy,
             @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir
     ) {
+        logger.info("Entering request to get all order data ");
         PageableResponse<OrderDto> orders = orderService.getOrders(pageNumber, pageSize, sortBy, sortDir);
+        logger.info("Completed request to get all order data ");
         return new ResponseEntity<>(orders, HttpStatus.OK);
     }
 
