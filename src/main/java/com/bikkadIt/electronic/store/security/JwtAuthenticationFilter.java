eb6f75af869617273d08e31fa0a62c5104d09b5a
supriyaphalle <supriyaphalle@gmail.com>
2023-12-23 22:40:02 +0530

    Added: JwtAuthenticationFilter file

---
+++ src/main/java/com/bikkadIt/electronic/store/security/JwtAuthenticationFilter.java
@@ -0,0 +1,82 @@
+package com.bikkadIt.electronic.store.security;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private Logger logger = LoggerFactory.getLogger(OncePerRequestFilter.class);
+
+    @Autowired
+    private JwtHelper jwtHelper;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String requestHeader = request.getHeader("Authorization");
+        logger.info("Header : {}", requestHeader);
+        String username = null;
+        String token = null;
+        if (requestHeader != null && requestHeader.startsWith("Brearer")) {
+
+            token = requestHeader.substring(7);
+            try {
+
+                username = this.jwtHelper.getUsernameFromToken(token);
+
+            } catch (IllegalArgumentException e) {
+                logger.info("Illegal argument while fetching the username !!");
+                e.printStackTrace();
+            } catch (ExpiredJwtException e) {
+                logger.info("Given jwt token is expired !!");
+                e.printStackTrace();
+            } catch (MalformedJwtException e) {
+                logger.info("Some changed has done in token ....Invalid token");
+                e.printStackTrace();
+            } catch (Exception e) {
+
+            }
+
+
+        } else {
+            logger.info("Invalid Header Value !!");
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+            Boolean validateToken = this.jwtHelper.validateToken(token, userDetails);
+            if (validateToken) {
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                logger.info("validation fails!! ");
+            }
+
+            filterChain.doFilter(request, response);
+
+        }
+
+
+    }
+}
