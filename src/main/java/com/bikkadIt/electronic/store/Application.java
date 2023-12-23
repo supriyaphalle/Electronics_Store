1ae64ea778d7b3916c77f41037d11c0d2cad35fb
supriyaphalle <supriyaphalle@gmail.com>
2023-12-23 20:37:39 +0530

    Added: implements command line runner in application file

--- src/main/java/com/bikkadIt/electronic/store/Application.java
+++ src/main/java/com/bikkadIt/electronic/store/Application.java
@@ -1,13 +1,22 @@
 package com.bikkadIt.electronic.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	@Override
+	public void run(String... args) throws Exception {
+	System.out.println("Encoded password is " +passwordEncoder.encode("supriya"));
+	}
 }
