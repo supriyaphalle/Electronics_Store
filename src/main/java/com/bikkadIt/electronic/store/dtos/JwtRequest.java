9000ec24103acfa9f62e6f835c7ff6c2dfa84d1c
supriyaphalle <supriyaphalle@gmail.com>
2023-12-26 21:59:52 +0530

    Added: JwtRequest and JwtResponse

---
+++ src/main/java/com/bikkadIt/electronic/store/dtos/JwtRequest.java
@@ -0,0 +1,16 @@
+package com.bikkadIt.electronic.store.dtos;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JwtRequest {
+
+    private String email;
+
+    private String password;
+
+}
---
+++ src/main/java/com/bikkadIt/electronic/store/dtos/JwtResponse.java
@@ -0,0 +1,16 @@
+package com.bikkadIt.electronic.store.dtos;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JwtResponse {
+
+    private String jwtToken;
+
+    private UserDto user;
+
+}
