ee993d989b9a8275d617cc948868cf5fd0442ab5
supriyaphalle <supriyaphalle@gmail.com>
2023-12-27 11:19:00 +0530

    Added: Testcase for categoryController

--- src/test/java/com/bikkadIt/electronic/store/controller/CategoryControllerTest.java
+++ src/test/java/com/bikkadIt/electronic/store/controller/CategoryControllerTest.java
@@ -2,10 +2,14 @@ package com.bikkadIt.electronic.store.controller;
 
 import com.bikkadIt.electronic.store.dtos.CategoryDto;
 import com.bikkadIt.electronic.store.dtos.PageableResponse;
+import com.bikkadIt.electronic.store.dtos.ProductDto;
 import com.bikkadIt.electronic.store.entities.Category;
+import com.bikkadIt.electronic.store.entities.Product;
 import com.bikkadIt.electronic.store.services.CategoryService;
 import com.bikkadIt.electronic.store.services.FileService;
+import com.bikkadIt.electronic.store.services.ProductService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -15,10 +19,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.multipart.MultipartFile;
+
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -28,6 +38,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Date;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -38,6 +49,10 @@ public class CategoryControllerTest {
 
     @MockBean
     FileService fileService;
+
+    @MockBean
+    ProductService productService;
+
     Category category;
     @Autowired
     private ModelMapper mapper;
@@ -51,6 +66,15 @@ public class CategoryControllerTest {
                 .description("This is test case ")
                 .coverImage("pqrs.png")
                 .build();
+    }
+
+    private String convertObjectToJsonString(Category category) {
+        try {
+            return new ObjectMapper().writeValueAsString(category);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Test
@@ -70,14 +94,6 @@ public class CategoryControllerTest {
 
     }
 
-    private String convertObjectToJsonString(Category category) {
-        try {
-            return new ObjectMapper().writeValueAsString(category);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
 
     @Test
     public void updateCategoryTest() throws Exception {
@@ -94,8 +110,6 @@ public class CategoryControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.title").exists());
     }
-
-
 
     @Test
     public void getAllCategoryTest() throws Exception {
@@ -138,6 +152,133 @@ public class CategoryControllerTest {
 
     }
 
+
+    @Test
+    public void deleteCategoryTest() throws Exception {
+        String categoryId = "abcd";
+
+        Mockito.doNothing().when(this.categoryService).deleteCategory(categoryId);
+
+        this.mockMvc.perform(
+                MockMvcRequestBuilders.delete(("/category/" + categoryId))
+
+        ).andDo(print()).andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void updateCategoryOfProductTest() throws Exception {
+        Product product = Product.builder().name("Mobile").price(15000).stock(true).live(true)
+                .productImage("pqr.png").quantity(13).category(category).discountPrice(14000)
+                .addedDate(new Date()).description("This is Product service test").build();
+        ProductDto productDto = mapper.map(product, ProductDto.class);
+
+        String productId = "abcd";
+        String categoryId = "ssss";
+        Mockito.when(productService.updateCategory(productId, categoryId)).thenReturn(productDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/category/" + categoryId + "/products/" + productId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+//                .andExpect(jsonPath().exists());
+    }
+
+    @Test
+    public void createProductWithCategoryTest() throws Exception {
+
+        Product product = Product.builder().name("Mobile").price(15000).stock(true).live(true)
+                .productImage("pqr.png").quantity(13).category(category).discountPrice(14000)
+                .addedDate(new Date()).description("This is Product service test").build();
+        ProductDto productDto = mapper.map(product, ProductDto.class);
+
+        String productId = "abcd";
+        String categoryId = "ssss";
+        Mockito.when(productService.createWithCategory(productDto, categoryId)).thenReturn(productDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/category/" + categoryId + "/products/" + productId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+
+//    public void testGetUserByName() throws Exception {
+//        String firstName = "Jack";
+//        String lastName = "s";
+//        this.userClientObject = client.createClient();
+//        mockMvc.perform(get("/byName")
+//                        .sessionAttr("userClientObject", this.userClientObject)
+//                        .param("firstName", firstName)
+//                        .param("lastName", lastName)
+//                ).andDo(print())
+//                .andExpect(status().isOk())
+//                .andExpect(content().contentType("application/json"))
+//                .andExpect(jsonPath("$[0].id").exists())
+//                .andExpect(jsonPath("$[0].fn").value("Marge"));
+//    }
+
+
+    @Test
+    public void uploadImageTest() throws Exception {
+        String categoryId = "ssss";
+
+        MockMultipartFile categoryImage = new MockMultipartFile("file", "38b610cc-b181-4a4a-a87f-769d63bd7489.jpg",
+                "image/jpg", "Some data".getBytes());
+
+
+        MvcResult result = this.mockMvc.perform(multipart("/category/image/" + categoryId+"categoryImage?")
+
+                        .file(categoryImage))
+                .andExpect(status().isOk())
+                .andReturn();
+        Assertions.assertEquals(200, result.getResponse().getStatus());
+
+    }
+
+    @Test
+    public void getImageFromServer() throws Exception {
+
+        MockMultipartFile imageFile = new MockMultipartFile("file","1.png","image/png","Some data".getBytes());
+
+        MvcResult result = this.mockMvc.perform(multipart("/category/")
+                        .file(imageFile)
+                        .accept(MediaType.APPLICATION_JSON)
+
+                )
+                .andReturn();
+        Assertions.assertEquals(200,result.getResponse().getStatus());
+
+    }
+
+
+//    @Test
+//    void givenImageAsMultipart_shouldReturnImageViewURL() throws Exception {
+//        MockMultipartFile imageFile = new MockMultipartFile("file","1.png",
+//                "image/png","Some data".getBytes());
+//        MvcResult result = this.mockMvc.perform(multipart("/admin/uploadimage")
+//                        .file(imageFile))
+//                .andReturn();
+//        Assert.assertEquals(200,result.getResponse().getStatus());
+//    }
+//
+//    @Test
+//    void givenFileName_WhenFound_ReturnsFile() throws Exception {
+//
+//        String fileName="notebook.jpg";
+//        String imagePath="//src//main//resources//Images//";
+//        String fileBasePath = System.getProperty("user.dir")+imagePath;
+//        Path path = Paths.get(fileBasePath + fileName);
+//        Resource resource = new UrlResource(path.toUri());
+//        Mockito.when(adminService.loadFile(any(), any())).thenReturn(resource);
+//        MvcResult result = this.mockMvc.perform(get("/admin/downloadfile/fileName?fileName=imageName"))
+//                .andReturn();
+//        Assert.assertEquals(200,result.getResponse().getStatus());
+//    }
+
+
 //    @Test
 //    public void postCategoryImageByID() throws Exception {
 //        String categoryId = "qazxsw";
@@ -170,7 +311,6 @@ public class CategoryControllerTest {
 //
 //
 //    }
-
 
 
 }
